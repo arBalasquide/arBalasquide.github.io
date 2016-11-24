0e4921d3769b4a06f654fe46b80654f59ff4687e
arBalasquide <adrian.balasquide@gmail.com>
2016-11-23 22:34:01 -0400

    Changed font and made typing faster

--- 1337/main.js
+++ 1337/main.js
@@ -1,8 +1,10 @@
-var message =  "Hello, stranger. Welcome to the unknown.";
+var message =  "Hello, stranger. Welcome to the unknown, where you will find nothing.";
 
 var hold = "";
 var count = 0;
 var arrayMessage = message.split(''); 
+
+var c = "C:/"
 
 function typeMessage() {
 
@@ -10,11 +12,11 @@ function typeMessage() {
 	var res = hold.concat(temp);
 	hold = res;
 	
-	document.getElementById('message').innerHTML = res;		
+	document.getElementById('message').innerHTML = c.concat(res);		
 	
 	count++;
 	
-	if(count < arrayMessage.length)setTimeout(typeMessage, 150);
+	if(count < arrayMessage.length)setTimeout(typeMessage, 100);
 }
 
 window.onload(typeMessage());	
