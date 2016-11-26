b03ddbb6a7391cb333fbc9ba8bacf0dfbca47c02
arBalasquide <adrian.balasquide@gmail.com>
2016-11-26 12:56:29 -0400

    Making main.js more efficient

--- 1337/main.js
+++ 1337/main.js
@@ -1,21 +1,17 @@
 var messages =  ["Hello, stranger. Welcome to the unknown, where you will find nothing.", "What do you want me to do?", "Hmm, it might be possible"];
 
-var x = 0;
-
 var hold = "";
 var mCount = 0;
 var fCount = 0;
 
-var c = "C:/"
-
 function typeMessage() {
 
-	for(k = 0; k < messages.length - 1; k++){
+	for(k = 0; k < messages.length; k++){
 	
 		message = messages[fCount];
 		arrayMessage = message.split('');
-		var temp = arrayMessage[mCount];
-		var res = hold.concat(temp);
+		var temp = arrayMessage[k];
+		var res = hold + temp;
 		hold = res;
 			
 		var messageEl = $("<div/>").addClass("message");
